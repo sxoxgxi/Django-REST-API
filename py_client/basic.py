edb1b358008fb878911bc14aaf16c2e6963262a7
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-24 20:27:27 +0545

    hello world

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,7 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000"
+endpoint = "http://127.0.0.1:8000/api"
 
 response = requests.get(endpoint)
 print(response.status_code)
+print(response.json()['message'])
