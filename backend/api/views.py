a865ad238455e867382305103c1a8b0e084130c3
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-25 10:49:55 +0545

    GET

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,14 @@
+import contextlib
+import json
 from django.http import JsonResponse
 
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hello world"})
+    body = request.body
+    data = {}
+    with contextlib.suppress(Exception):
+        data = json.loads(body)
+    data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api"
 
-response = requests.get(endpoint)
-print(response.status_code)
-print(response.json()['message'])
+response = requests.get(endpoint, params={"abc": 123}, json={'name': 'nirmal'})
+# print(response.status_code)
+print(response.json())
