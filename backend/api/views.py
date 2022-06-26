800f0d33e614651d4053fa27af7686010ed94e86
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-26 17:19:31 +0545

    django view to drf view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,19 @@
-# import contextlib
-# import json
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 from product.models import Product
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     if model_data := Product.objects.all().order_by('?').first():
         data = model_to_dict(model_data)
     else:
-        data = {}
-    return JsonResponse(data)
+        data = {"Error": "Product not available"}
+    return Response(data)
