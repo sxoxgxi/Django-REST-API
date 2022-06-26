e35ca033fcc550547da4a5160930cc5a2e2a12fb
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-26 17:19:48 +0545

    django view to drf view

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'product.apps.ProductConfig'
+    'product.apps.ProductConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
