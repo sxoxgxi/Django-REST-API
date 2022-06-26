a345408d04cb1ac207e006c06337eba404612e19
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-26 16:57:40 +0545

    model_to_dick

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,14 @@
-import contextlib
-import json
+# import contextlib
+# import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from product.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
-    data = {}
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+    if model_data := Product.objects.all().order_by('?').first():
+        data = model_to_dict(model_data)
+    else:
+        data = {}
     return JsonResponse(data)
